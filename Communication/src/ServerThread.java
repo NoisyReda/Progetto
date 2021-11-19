d110d68bfcfc76a13d0494a4ad7795b55452a8c1
NoisyReda <81413520+NoisyReda@users.noreply.github.com>
2021-11-19 08:29:32 +0100

    Add files via upload

---
+++ Communication/src/ServerThread.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Redaelli_Mattia03
+ */
+public class ServerThread extends Thread{
+private int ports[] =
+    
+    @Override
+    public void run() {
+      
+        
+    }
+    
+}
